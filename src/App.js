0f0022790b5b20efaf58b2dd0e7280e53e0683f9
gnganpath <esourcej@gmail.com>
2020-04-08 12:27:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and AWS Amplify console with react
         </a>
       </header>
     </div>
